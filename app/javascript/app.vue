8eefb8c9445f18bf4db2d2c77748626f47adc0dd
GriffinJ <jrgriffiniii@gmail.com>
2018-11-16 09:56:49 -0500

    [WIP] Migrating the PUL branding; Integrating WebPacker and LUX

---
+++ app/javascript/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
