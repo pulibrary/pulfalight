15359d728759339e7d7adc810a568d089c02491b
GriffinJ <jrgriffiniii@gmail.com>
2019-02-26 09:19:49 -0500

    Fixing custom document behavior

--- lib/pulfa/custom_component.rb
+++ lib/pulfa/custom_component.rb
@@ -2,5 +2,17 @@
 module Pulfa
   class CustomComponent < Arclight::CustomComponent
     include IndexingBehavior
+
+    def initialize
+      super
+      @dao_elements = {}
+      @digital_objects = {}
+    end
+
+    private
+
+      def dao_elements(prefix = "/")
+        @dao_elements[prefix] ||= ng_xml.xpath("#{prefix}/dao[@href]").to_a
+      end
   end
 end
