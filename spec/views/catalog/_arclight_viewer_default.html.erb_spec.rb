a6c0d3d28fd26c7e2b8299a9426b23da0d4a3d1a
Trey Pendragon <tpendragon@princeton.edu>
2019-10-30 13:40:50 -0700

    Beginning of a view spec.
    Co-authored-by: Anna Headley <anna3lc@gmail.com>

---
+++ spec/views/catalog/_arclight_viewer_default.html.erb_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+# Strategy: Implement an Arclight::Viewer using documentation at
+# https://github.com/projectblacklight/arclight/wiki/Digital-Object-Viewers and
+# make sure it renders here.
+RSpec.describe "catalog/_arclight_viewer_default.html.erb" do
+end
