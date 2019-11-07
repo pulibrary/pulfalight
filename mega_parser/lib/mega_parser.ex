c7f4638b9d415ddb58062f78f95a0f3d70dd0eb0
Trey Pendragon <tpendragon@princeton.edu>
2019-11-07 16:05:58 -0500

    Work off a standard template that gets transformed.

--- mega_parser/lib/mega_parser.ex
+++ mega_parser/lib/mega_parser.ex
@@ -34,17 +34,17 @@ defmodule MegaParser do
     parsed_file
     |> Meeseeks.all(%Selector.Component{})
     |> Enum.map(&convert_component(&1, parent_record))
+    |> Enum.map(&convert_standard_component(&1, parent_record))
     |> insert_sort
-    |> Enum.map(&process_component(&1, parent_record))
   end
 
   defp convert_component(component, parent_record) do
     %{
-      ref_ssi: component |> Meeseeks.attr("id"),
-      has_online_content_ssim: [component |> Meeseeks.one(xpath(".//dao")) != nil],
-      geogname_sim: component |> extract_text("./controllaccess/geogname"),
-      level_ssm: component |> extract_level,
-      component_level_isim: [
+      id: component |> Meeseeks.attr("id"),
+      has_online_content: [component |> Meeseeks.one(xpath(".//dao")) != nil],
+      geogname: component |> extract_text("./controllaccess/geogname"),
+      level: component |> extract_level,
+      component_level: [
         component
         |> Meeseeks.all(%Meeseeks.Selector.Element{
           combinator: %Meeseeks.Selector.Combinator.AncestorsOrSelf{
@@ -53,9 +53,24 @@ defmodule MegaParser do
         })
         |> length
       ],
-      containers_ssim: component |> Meeseeks.all(xpath("./did/container")) |> container_string,
-      parent_ssim: component |> get_parents(parent_record)
+      containers: component |> Meeseeks.all(xpath("./did/container")) |> container_string,
+      parent_ids: component |> get_parents(parent_record)
     }
+  end
+
+  defp convert_standard_component(component, parent_record) do
+    %{}
+    |> Map.put(:id, "#{parent_record.id}#{component.id}")
+    |> Map.put(:ref_ssi, component.id)
+    |> Map.put(:ead_ssi, parent_record.ead_ssi)
+    |> put_multiple([:level_ssm, :level_sim], component.level)
+    |> put_multiple([:geogname_ssm, :geogname_sim], component.geogname)
+    |> Map.put(:component_level_isim, component.component_level)
+    |> Map.put(:parent_ssim, component.parent_ids)
+    |> Map.put(:parent_ssi, component.parent_ids |> Enum.slice(-1..-1))
+    |> Map.put(:has_online_content_ssim, component.has_online_content)
+    |> Map.put(:collection_unitid_ssm, parent_record.unitid_ssm)
+    |> Map.put(:containers_ssim, component.containers)
   end
 
   def get_parents(component, parent_record) do
@@ -102,16 +117,6 @@ defmodule MegaParser do
     |> container_string
   end
 
-  defp process_component(component, parent_record) do
-    component
-    |> Map.put(:id, "#{parent_record.id}#{component.ref_ssi}")
-    |> Map.put(:ead_ssi, parent_record.ead_ssi)
-    |> Map.put(:geogname_ssm, component.geogname_sim)
-    |> Map.put(:collection_unitid_ssm, parent_record.unitid_ssm)
-    |> Map.put(:level_sim, component.level_ssm)
-    |> Map.put(:parent_ssi, component.parent_ssim |> Enum.slice(-1..-1))
-  end
-
   defp extract_text(ead, xpath) do
     ead |> Meeseeks.all(xpath(xpath)) |> Enum.map(&Meeseeks.text/1)
   end
@@ -121,16 +126,23 @@ defmodule MegaParser do
     |> Saxy.parse_stream(MegaParser.SaxParser, [])
     |> elem(1)
     |> Map.get(:document)
-    |> convert_standard
+    |> convert_standard_parent
   end
 
-  defp convert_standard(document) do
+  defp convert_standard_parent(document) do
     %{}
     |> Map.put(:id, document.id |> String.replace(".", "-"))
     |> Map.put(:ead_ssi, document.id)
     |> put_multiple([:title_ssm, :title_teim], document.title)
     |> put_multiple([:level_sim], document.level)
     |> Map.put(:level_ssm, ["collection"])
+    |> Map.put(:unitid_ssm, document[:unitid])
+    |> Map.put(:normalized_title_ssm, [normalized_title(document)])
+    |> Map.put(:normalized_date_ssm, normalized_date(document))
+    |> Map.put(:unitdate_bulk_ssim, document[:unitdate_bulk])
+    |> Map.put(:unitdate_inclusive_ssm, document[:unitdate_inclusive])
+    |> Map.put(:unitdate_other_ssim, document[:unitdate_other])
+    |> Map.put(:date_range_sim, document[:unitdate_normal] |> MegaParser.YearRange.parse_range)
   end
 
   defp put_multiple(map, keys, value) do
@@ -147,17 +159,16 @@ defmodule MegaParser do
       id:
         ead
         |> Meeseeks.one(xpath("./eadheader/eadid"))
-        |> Meeseeks.text()
-        |> String.replace(".", "-"),
-      title_filing_si: ead |> extract_text("./eadheader/filedesc/titlestmt/titleproper[@type='filing']"),
-      title_ssm:
+        |> Meeseeks.text(),
+      title_filing: ead |> extract_text("./eadheader/filedesc/titlestmt/titleproper[@type='filing']"),
+      title:
         ead |> Meeseeks.all(xpath("./archdesc/did/unittitle")) |> Enum.map(&Meeseeks.text/1),
       ead_ssi: ead |> Meeseeks.one(xpath("./eadheader/eadid")) |> Meeseeks.text(),
-      unitdate_ssm: ead |> extract_text("./archdesc/did/unitdate"),
-      unitdate_bulk_ssim: ead |> extract_text("./archdesc/did/unitdate[@type='bulk']"),
-      unitdate_inclusive_ssm: ead |> extract_text("./archdesc/did/unitdate[@type='inclusive']"),
-      unitdate_other_ssim: ead |> extract_text("./archdesc/did/unitdate[not(@type)]"),
-      unitid_ssm: ead |> extract_text("./archdesc/did/unitid"),
+      unitdate: ead |> extract_text("./archdesc/did/unitdate"),
+      unitdate_bulk: ead |> extract_text("./archdesc/did/unitdate[@type='bulk']"),
+      unitdate_inclusive: ead |> extract_text("./archdesc/did/unitdate[@type='inclusive']"),
+      unitdate_other: ead |> extract_text("./archdesc/did/unitdate[not(@type)]"),
+      unitid: ead |> extract_text("./archdesc/did/unitid"),
       geogname_ssm: ead |> extract_text("./archdesc/controlaccess/geogname"),
       creator_ssm: ead |> extract_text("./archdesc/did/origination"),
       creator_persname_ssm: ead |> extract_text("./archdesc/did/origination/persname"),
@@ -174,48 +185,48 @@ defmodule MegaParser do
         ),
       extent_ssm: ead |> extract_text("./archdesc/did/physdesc/extent"),
       genreform_sim: ead |> extract_text("./archdesc/controlaccess/genreform"),
-      level_sim: ead |> Meeseeks.one(xpath("./archdesc")) |> extract_level,
-      date_range_sim: ead |> Meeseeks.one(xpath("./did/unitdate")) |> Meeseeks.attr("normal") |> MegaParser.YearRange.parse_range
+      level: ead |> Meeseeks.one(xpath("./archdesc")) |> extract_level,
+      unitdate_normal: ead |> Meeseeks.one(xpath("./did/unitdate")) |> Meeseeks.attr("normal")
     }
-    |> process_parent_record
+    |> convert_standard_parent
   end
 
   defp normalized_title(record) do
-    record.title_ssm ++ normalized_date(record) |> Enum.join(", ")
+    record.title ++ normalized_date(record) |> Enum.join(", ")
   end
 
   def normalized_date(record) do
-    [MegaParser.NormalizedDate.to_string(record.unitdate_inclusive_ssm, record.unitdate_bulk_ssim |> Enum.at(0), record.unitdate_other_ssim |> Enum.at(0))]
+    [MegaParser.NormalizedDate.to_string(record.unitdate_inclusive, record.unitdate_bulk |> Enum.at(0), record.unitdate_other |> Enum.at(0))]
   end
 
-  defp process_parent_record(record) do
-    record
-    |> Map.put(:normalized_title_ssm, [normalized_title(record)])
-    |> Map.put(:normalized_date_ssm, normalized_date(record))
-    |> Map.put(:level_ssm, ["collection"])
-    |> Map.put(:title_teim, record.title_ssm)
-    |> Map.put(:unitid_teim, record.unitid_ssm)
-    |> Map.put(:geogname_sim, record.geogname_ssm)
-    |> Map.put(:places_sim, record.geogname_ssm)
-    |> Map.put(:places_ssim, record.geogname_ssm)
-    |> Map.put(:places_ssm, record.geogname_ssm)
-    |> Map.put(:creator_sim, record.creator_ssm)
-    |> Map.put(:creator_ssim, record.creator_ssm)
-    |> Map.put(:creator_sort, Enum.join(record.creator_ssm, ", "))
-    |> Map.put(:creator_persname_ssim, record.creator_persname_ssm)
-    |> Map.put(:creator_corpname_ssim, record.creator_corpname_ssm)
-    |> Map.put(:creator_corpname_sim, record.creator_corpname_ssm)
-    |> Map.put(:creator_famname_ssim, record.creator_famname_ssm)
-    |> Map.put(
-      :creators_ssim,
-      record[:creator_persname_ssm] ++
-        record[:creator_corpname_ssm] ++ record[:creator_famname_ssm]
-    )
-    |> Map.put(:acqinfo_ssm, record[:acqinfo_ssim])
-    |> Map.put(:access_subjects_ssm, record[:access_subjects_ssim])
-    |> Map.put(:extent_teim, record[:extent_ssm])
-    |> Map.put(:genreform_ssm, record[:genreform_sim])
-  end
+  # defp process_parent_record(record) do
+  #   record
+  #   |> Map.put(:normalized_title_ssm, [normalized_title(record)])
+  #   |> Map.put(:normalized_date_ssm, normalized_date(record))
+  #   |> Map.put(:level_ssm, ["collection"])
+  #   |> Map.put(:title_teim, record.title_ssm)
+  #   |> Map.put(:unitid_teim, record.unitid_ssm)
+  #   |> Map.put(:geogname_sim, record.geogname_ssm)
+  #   |> Map.put(:places_sim, record.geogname_ssm)
+  #   |> Map.put(:places_ssim, record.geogname_ssm)
+  #   |> Map.put(:places_ssm, record.geogname_ssm)
+  #   |> Map.put(:creator_sim, record.creator_ssm)
+  #   |> Map.put(:creator_ssim, record.creator_ssm)
+  #   |> Map.put(:creator_sort, Enum.join(record.creator_ssm, ", "))
+  #   |> Map.put(:creator_persname_ssim, record.creator_persname_ssm)
+  #   |> Map.put(:creator_corpname_ssim, record.creator_corpname_ssm)
+  #   |> Map.put(:creator_corpname_sim, record.creator_corpname_ssm)
+  #   |> Map.put(:creator_famname_ssim, record.creator_famname_ssm)
+  #   |> Map.put(
+  #     :creators_ssim,
+  #     record[:creator_persname_ssm] ++
+  #       record[:creator_corpname_ssm] ++ record[:creator_famname_ssm]
+  #   )
+  #   |> Map.put(:acqinfo_ssm, record[:acqinfo_ssim])
+  #   |> Map.put(:access_subjects_ssm, record[:access_subjects_ssim])
+  #   |> Map.put(:extent_teim, record[:extent_ssm])
+  #   |> Map.put(:genreform_ssm, record[:genreform_sim])
+  # end
 
   def extract_level(level_node) do
     level = level_node |> Meeseeks.attr("level")
