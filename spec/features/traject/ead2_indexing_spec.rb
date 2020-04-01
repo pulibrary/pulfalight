b40b8a7df3e2303869e7062cda4659059f7772d7
GriffinJ <jrgriffiniii@gmail.com>
2020-04-01 16:13:15 -0400

    Ensuring that component elements (<c>) with nested children have child components inserted into the "component" field of the Solr Document at the component level (rather than just the collection-level)

--- spec/features/traject/ead2_indexing_spec.rb
+++ spec/features/traject/ead2_indexing_spec.rb
@@ -43,6 +43,25 @@ describe "EAD 2 traject indexing", type: :feature do
     ENV["REPOSITORY_ID"] = nil
   end
 
+  context "with nested components in a collection" do
+    let(:fixture_path) do
+      Rails.root.join("spec", "fixtures", "ead", "mudd", "publicpolicy", "MC221.EAD.xml")
+    end
+
+    it "indexes child components for the collection components" do
+      components = result["components"]
+      parent_components = components.select { |c| c["components"] }
+      expect(parent_components).not_to be_empty
+      parent_component = parent_components.first
+      child_components = parent_component["components"]
+      expect(child_components.length).to eq(52)
+      child_component = child_components.first
+      expect(child_component).to include("id" => ["MC221_c0002"])
+      expect(child_component).to include("component_level_isim" => [2])
+      expect(child_component).to include("level_ssm" => ["File"])
+    end
+  end
+
   describe "solr fields" do
     it "id" do
       expect(result["id"].first).to eq "MC221"
