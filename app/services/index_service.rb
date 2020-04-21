5106c28e2c3460afd0f2e10a2764e42266ef6fe5
GriffinJ <jrgriffiniii@gmail.com>
2020-04-21 15:00:04 -0400

    Rebasing and updating the service class

--- app/services/index_service.rb
+++ app/services/index_service.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class IndexService
+  def self.pulfa_root
+    Rails.root.join("eads", "pulfa")
+  end
+
   # Index an EAD-XML Document into Solr
   # @param [String] relative_path
   def index_document(relative_path:, root_path: nil)
-    root_path ||= pulfa_root
+    root_path ||= self.class.pulfa_root
     ead_file_path = File.join(root_path, relative_path)
     IndexJob.perform_later([ead_file_path])
   end
