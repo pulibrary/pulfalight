3136bf8556a44874d5538fbe993c1b705459510a
Eliot Jordan <eliotj@princeton.edu>
2020-09-24 21:17:59 -0500

    Don't show home page header on show pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module ApplicationHelper
   def on_home_page?
-    controller_name == "catalog" && !has_search_parameters?
+    controller_name == "catalog" && params[:action] == "index" && !has_search_parameters?
   end
 end
