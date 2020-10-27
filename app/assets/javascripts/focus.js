346246059f25b701ffc0a1195a459c07cf757d93
Shaun Ellis <shaun@sdellis.com>
2020-10-27 16:02:48 -0400

    cleans up javascript

--- app/assets/javascripts/focus.js
+++ app/assets/javascripts/focus.js
@@ -1,6 +1,4 @@
-//Attempt to get the element using document.getElementById
 $( document ).ready(function() {
-  // var q = document.getElementById("q");
   var q = $( ".home-header #q" )
   if(typeof(q) != 'undefined' && q != null){
       // Set focus on the search box if it exists
