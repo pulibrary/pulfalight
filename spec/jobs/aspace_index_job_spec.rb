d78f405cbe1ba49b246df698d1659f59fbc8eda7
Trey Pendragon <tpendragon@princeton.edu>
2020-11-24 15:12:54 -0800

    Don't index unpublished EADs
    Closes #281

--- spec/jobs/aspace_index_job_spec.rb
+++ spec/jobs/aspace_index_job_spec.rb
@@ -9,12 +9,26 @@ RSpec.describe AspaceIndexJob do
         stub_aspace_login
         stub_aspace_ead(resource_descriptions_uri: "repositories/13/resources/5396", ead: "mss/C1588.xml")
 
+        connection.delete_by_query("id:C1588test")
         described_class.perform_now(resource_descriptions_uri: "repositories/13/resources/5396", repository_id: "mss")
         connection.commit
 
         items = connection.get("select", params: { q: "id:C1588test" })
         expect(items["response"]["numFound"]).to eq 1
         expect(items["response"]["docs"].first["repository_ssm"]).to eq ["Manuscripts Division"]
+      end
+    end
+    context "when given an internal EAD" do
+      it "doesn't index it" do
+        stub_aspace_login
+        stub_aspace_ead(resource_descriptions_uri: "repositories/13/resources/5396", ead: "mss/C1588-internal.xml")
+
+        connection.delete_by_query("id:C1588testinternal")
+        described_class.perform_now(resource_descriptions_uri: "repositories/13/resources/5396", repository_id: "mss")
+        connection.commit
+
+        items = connection.get("select", params: { q: "id:C1588testinternal" })
+        expect(items["response"]["numFound"]).to eq 0
       end
     end
   end
