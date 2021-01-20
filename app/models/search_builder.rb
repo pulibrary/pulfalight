13eacb8b164b3e0edc426fb22ebabfb37017d4ec
Trey Pendragon <tpendragon@princeton.edu>
2021-01-20 14:37:39 -0800

    Boost collections in search results
    Work towards #560

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -4,11 +4,16 @@ class SearchBuilder < Blacklight::SearchBuilder
   include Arclight::SearchBehavior
   include BlacklightRangeLimit::RangeLimitBuilder
 
-  self.default_processor_chain += [:remove_grouping]
+  self.default_processor_chain += [:remove_grouping, :boost_collections]
 
   def remove_grouping(solr_params)
     # Remove grouping parameters if faceting by collection
     Arclight::Engine.config.catalog_controller_group_query_params.keys.each { |k| solr_params.delete(k) } if blacklight_params.dig(:f, :collection_sim)
     solr_params
+  end
+
+  def boost_collections(solr_params)
+    solr_params[:bq] ||= []
+    solr_params[:bq] << "level_ssm:collection^5"
   end
 end
