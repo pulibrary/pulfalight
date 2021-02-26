8e2bd2da0d05abc08e43641568d5be614f28d07a
Bess Sadler <bess.sadler@princeton.edu>
2021-02-26 15:24:28 -0500

    Add instructions to correction modal
    Include a link to Archival Language document

---
+++ spec/views/catalog/_correction_modal.html.erb_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe "catalog/_correction_modal.html.erb" do
+  let(:document) do
+    {
+      "ead_ssi" => "MC001.02.06",
+      "id" => ["MC001-02-06"],
+      "repository_code_ssm" => ["fake repository code"]
+
+    }
+  end
+  let(:solr_document) { SolrDocument.new(document) }
+
+  it "renders the suggest a correction modal, with instructions" do
+    assign :document, solr_document
+    render
+    content = view.content_for(:correction_modal)
+    expect(content).to have_content("Please use this area to report errors or omissions")
+    expect(content).to have_link("harmful or offensive language", href: "/archival_language")
+  end
+end
