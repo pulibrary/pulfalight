9b3ead9f361e3ed2d32a05fe7db2fe9deea2e24a
Bess Sadler <bess.sadler@princeton.edu>
2021-03-08 15:00:58 -0500

    Pre-work for running system specs later
    We will need to re-write some of our feature tests to run as system specs, and that's not going to fit in the current work window, but setting this up so we have it in place later.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,7 +38,7 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :system, js: true) do
-    driven_by :selenium_chrome_headless
+    driven_by :selenium
   end
 
   config.infer_spec_type_from_file_location!
