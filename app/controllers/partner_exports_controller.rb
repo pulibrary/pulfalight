6fd3fda2573784724cb012e72a8c77f47b5d7ae9
Trey Pendragon <tpendragon@princeton.edu>
2021-05-03 17:13:34 -0700

    Add PACSCL page.

---
+++ app/controllers/partner_exports_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+class PartnerExportsController < ApplicationController
+  # PACSCL just needs straight links to the XML for every collection, but with
+  # no containers.
+  def pacscl
+    @collections = collections
+    render layout: false
+  end
+
+  def collections
+    Blacklight.default_index.search(q: "level_ssm:collection", fl: "id, normalized_title_ssm", rows: 10_000)["response"]["docs"]
+  end
+end
