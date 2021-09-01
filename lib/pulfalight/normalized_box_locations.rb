c14b5dae46cc23017588920cc5d9f0524edbfbe1
Bess Sadler <bess.sadler@princeton.edu>
2021-09-01 11:25:07 -0400

    Summarize box locations at the collection level
    This is the "magic physloc". Find the location of each box, and
    summarize them into a human readable physical location.
    
    Ref #807

---
+++ lib/pulfalight/normalized_box_locations.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+module Pulfalight
+  # Class for normalizing box locations to assemble the Magic Physloc Note
+  # It accepts a hash that looks like this:
+  # {
+  #   "hsvm" => ["1", "323", "2", "3", "4", "5", "6"],
+  #   "mss" => ["12", "20", "21", "30", "31"],
+  #   "rcpxm" => ["266", "114", "105", "106"]
+  # }
+  # ... and turns it into a string that looks like this:
+  # "This collection is stored in multiple locations:
+  # Firestone Library Vault: Boxes 1-6; 323
+  #
+  class NormalizedBoxLocations
+    # @param [Hash] box_locations
+    def initialize(box_locations)
+      @normalized_locations = {}
+
+      box_locations.keys.each do |location|
+        translated_location = "#{Pulfalight::LocationCode.resolve(location)} (#{location})"
+        @normalized_locations[translated_location] = calculate_box_ranges(box_locations[location])
+      end
+    end
+
+    def locations
+      @normalized_locations.keys
+    end
+
+    # Given a location code, return the relevent box ranges
+    def ranges_for(location_code)
+      key = @normalized_locations.keys.find { |a| a =~ /#{location_code}/ }
+      @normalized_locations[key]
+    end
+
+    ##
+    # Given an array of box ids:
+    # 1. Remove any that are not integers
+    # 2. Sort and group the integers into ranges
+    # 3. Consolidate any ranges that represent a single box
+    # 4. Re-add the non-integer box ids
+    # @param [<String>] box_numbers
+    # @return [<String>]
+    def calculate_box_ranges(box_numbers)
+      non_numeric_box_ids = box_numbers.reject { |a| a.to_i.to_s == a }
+      sorted = box_numbers.uniq.map(&:to_i).sort
+      ranges = []
+      first_number = nil
+
+      (1..sorted.max).each do |index|
+        first_number = index if sorted.include?(index) && first_number.nil?
+
+        if !sorted.include?(index) && !first_number.nil?
+          range = "#{first_number}-#{index - 1}"
+          ranges << range
+          first_number = nil
+        end
+
+        # In case the last number in the series is a singleton
+        if sorted.include?(index) && index == sorted.max
+          range = "#{first_number}-#{index}"
+          ranges << range
+        end
+      end
+      ranges.map { |a| consolidate_single_box_ranges(a) } | non_numeric_box_ids
+    end
+
+    def to_s
+      normalize
+    end
+
+    private
+
+    # Consolidate any box ranges that are only a single box
+    def consolidate_single_box_ranges(range)
+      box_numbers = range.split("-")
+      first = box_numbers.first
+      last = box_numbers.last
+      return first if first == last
+      range
+    end
+
+    # Generate a human readable summary of the box locations
+    # @return [String]
+    def normalize
+      message_strings = []
+      message_strings << "This collection is stored in multiple locations. "
+      @normalized_locations.keys.each do |location|
+        m = "#{location}: Boxes #{@normalized_locations[location].join('; ')}"
+        message_strings << m
+      end
+      message_strings.join(" ")
+    end
+  end
+end
---
+++ spec/lib/pulfalight/normalized_box_locations_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+require_relative "../../../lib/pulfalight/normalized_box_locations"
+
+describe Pulfalight::NormalizedBoxLocations do
+  describe ".new" do
+    let(:box_locations) do
+      {
+        "hsvm" => ["1", "323", "2", "3", "4", "5", "6", "221", "7", "8", "9", "10", "11", "13", "14", "15", "16", "17"],
+        "mss" => ["12", "20", "21", "P-000145", "292", "293"],
+        "rcpxm" => ["255", "266", "114", "105"]
+      }
+    end
+    let(:normalized_box_locations) { described_class.new(box_locations) }
+    let(:human_readable_box_locations) do
+      "This collection is stored in multiple locations.  Firestone Library (hsvm): Boxes 1-11; 13-17; 221; 323 Firestone Library (mss): Boxes 12; 20-21; 292-293; P-000145 ReCAP (rcpxm): Boxes 105; 114; 255; 266"
+    end
+
+    # Per conversation with
+    it "translates the location codes" do
+      expect(normalized_box_locations.locations).to contain_exactly("Firestone Library (hsvm)", "Firestone Library (mss)", "ReCAP (rcpxm)")
+    end
+
+    it "groups the boxes into human readable ranges" do
+      ranges = ["1-11", "13-17", "221", "323"]
+      expect(normalized_box_locations.ranges_for("hsvm")).to eq(ranges)
+    end
+
+    it "box ranges include non-numeric box numbers" do
+      ranges = ["12", "20-21", "292-293", "P-000145"]
+      expect(normalized_box_locations.ranges_for("mss")).to eq(ranges)
+    end
+
+    it "generates a human readable summary of the box locations" do
+      expect(normalized_box_locations.to_s).to eq human_readable_box_locations
+    end
+  end
+end
