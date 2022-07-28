537d4f48da57ea6b179a6d78aa228ceb9f372d7e
Anna Headley <anna.headley@gmail.com>
2022-07-28 11:36:14 -0400

    refactor
    Use a document all the way through, less parsing

--- app/models/pulfalight/document/xml_export.rb
+++ app/models/pulfalight/document/xml_export.rb
@@ -6,25 +6,24 @@ module Pulfalight::Document::XMLExport
 
   def export_as_xml
     content = client.get_xml(eadid: collection_unitid || unitid)
-    content = strip_containers(content) unless export_xml_containers?
-    content = add_pul_to_repository(content)
-    return content if collection?
-    document = Nokogiri::XML.parse(content).remove_namespaces!
+    document = Nokogiri::XML.parse(content)
+    document = strip_containers(document) unless export_xml_containers?
+    document = add_pul_to_repository(document)
+    return document if collection?
+    document = document.remove_namespaces!
     document.xpath("//*[@id='#{id}']")[0].to_xml
   end
 
-  def strip_containers(content)
-    document = Nokogiri::XML.parse(content)
+  def strip_containers(document)
     document.xpath("//xmlns:container").each(&:remove)
-    document.to_s
+    document
   end
 
-  def add_pul_to_repository(content)
-    document = Nokogiri::XML.parse(content)
+  def add_pul_to_repository(document)
     document.xpath("//xmlns:repository/xmlns:corpname").each do |node|
       node.inner_html = "Princeton University Library: #{node.text.strip}"
     end
-    document.to_s
+    document
   end
 
   def export_xml_containers?
