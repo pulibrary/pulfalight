b3486c01fed6f735f239a8811a5f0eaf6edbced5
Taylor Yamashita <taylory@princeton.edu>
2022-10-14 11:56:17 -0400

    Configure typeahead with hint=false
    WIP: We successfully overrode the upstream typeahead setup but for some
    reason it's querying /undefined.
    
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>

---
+++ app/assets/javascripts/typeahead_override.js
@@ -0,0 +1,38 @@
+Blacklight.onLoad(function() {
+  
+  'use strict';
+
+  $('[data-autocomplete-enabled="true"]').each(function() {
+    var $el = $(this);
+
+    // upstream code sets up typeahead and we don't want two of them
+    if($el.hasClass('tt-input')) {
+      $el.typeahead('destroy')
+    }
+
+    var suggestUrl = $el.data().autocompletePath;
+
+    var terms = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      remote: {
+        url: suggestUrl + '?q=%QUERY',
+        wildcard: '%QUERY'
+      }
+    });
+
+    terms.initialize();
+
+    // set hint to false
+    $el.typeahead({
+      hint: false,
+      highlight: true,
+      minLength: 2
+    },
+    {
+      name: 'terms',
+      displayKey: 'term',
+      source: terms.ttAdapter()
+    });
+  });
+});
