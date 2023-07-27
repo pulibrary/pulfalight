5c5a2ea294f93034be6a2b4ce1c7b63e358324c0
Anna Headley <anna.headley@gmail.com>
2023-07-27 13:00:44 -0400

    Uncomment passing test expectation

--- spec/features/traject/ead2_indexing_spec.rb
+++ spec/features/traject/ead2_indexing_spec.rb
@@ -491,7 +491,7 @@ describe "EAD 2 traject indexing", type: :feature do
 
             # Ensure it's inheriting from its parent.
             expect(component["scopecontent_combined_tsm"][0]).not_to eq "{}"
-            # expect(component["scopecontent_ssm"][0]).to include "harmful descriptions"
+            expect(component["scopecontent_ssm"][0]).to include "harmful descriptions"
             json = JSON.parse(component["scopecontent_combined_tsm"][0])
             expect(json["Content Warning"]).not_to be_blank
           end
