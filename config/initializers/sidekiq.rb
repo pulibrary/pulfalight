9c625e2ebfff00ac827b655cea3e318900fb74fb
Anna Headley <845363+hackartisan@users.noreply.github.com>
2023-10-31 16:40:20 -0400

    Update sidekiq configuration to not use redis client
    advances gh-1323

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
-require "redis"
-redis_config = YAML.safe_load(ERB.new(IO.read(Rails.root.join("config", "redis.yml"))).result, aliases: true)[Rails.env].with_indifferent_access
-redis_client = Redis.new(redis_config.merge(thread_safe: true))._client
-
+require_relative "redis_config"
 Sidekiq.configure_server do |config|
-  config.redis = redis_client.options
+  config.redis = { url: RedisConfig.url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = redis_client.options
+  config.redis = { url: RedisConfig.url }
 end
