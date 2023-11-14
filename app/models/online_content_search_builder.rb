2911216837a729ae4854f32073b2d9aaa58856d8
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2023-11-14 15:33:10 -0500

    Beginning to implement the OnlineContentSearchBuilder and modifying the Solr configuration with a custom requestHandler
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>
    Co-authored-by: Trey Pendragon <tpendragon@users.noreply.github.com>

---
+++ app/models/online_content_search_builder.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class OnlineContentSearchBuilder < SearchBuilder
+  self.default_processor_chain += [:switch_request_handler]
+
+  def switch_request_handler(solr_parameters)
+    solr_parameters[:qt] = "online_content"
+  end
+end
--- spec/requests/multiple_algorithms_spec.rb
+++ spec/requests/multiple_algorithms_spec.rb
@@ -2,26 +2,27 @@
 require "rails_helper"
 
 describe "multiple algorithms", type: :request do
-  describe 'search algorithm selection' do
+  describe "search algorithm selection" do
     before do
       allow(Pulfalight).to receive(:multiple_algorithms_enabled?).and_return(true)
     end
 
     context "when the search_algorithm parameter is not present" do
       it "ranks using the default request handler" do
-        get "/catalog.json?q=drawings"
+        get "/catalog.json?q=diary&search_field=all_fields"
         json = JSON.parse(response.body)
 
-        expect(json["data"][0]["attributes"]["title"]).to eq "Ogonek : roman / ."
+        expect(json["data"][0]["id"]).to eq "C1387"
       end
     end
 
     context "when the search_algorithm parameter is set to 'online_content'" do
       it "ranks using the online_content request handler" do
-        get "/catalog.json?q=drawings&search_algorithm=online_content"
+        # Expected: /catalog.json?f%5Bhas_direct_online_content_ssim%5D%5B%5D=online&q=diary&search_field=all_fields
+        get "/catalog.json?q=diary&search_field=all_fields&search_algorithm=online_content"
         json = JSON.parse(response.body)
 
-        expect(json["data"][0]["attributes"]["title"]).to eq "Reconstructing the Vitruvian Scorpio: An Engineering Analysis of Roman Field Artillery"
+        expect(json["data"][0]["id"]).to eq "MC221_c0094"
       end
     end
   end
