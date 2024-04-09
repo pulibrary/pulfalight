dd20672967df3f57e1ae1c12c9bb15bd6ac103a4
Eliot Jordan <eliotj@princeton.edu>
2024-04-09 13:41:30 -0500

    Add standardjs

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,10 +1,10 @@
 /* eslint no-console:0 */
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-import PulfalightLoader from "@/pulfalight/pulfalight_loader.es6"
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+import PulfalightLoader from '@/pulfalight/pulfalight_loader.es6'
 
 document.addEventListener('DOMContentLoaded', () => {
   // Load components
-  const loader = new PulfalightLoader
+  const loader = new PulfalightLoader()
   loader.run()
 })
