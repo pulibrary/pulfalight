459638c5f2f1b7b12e83439d79092f61510754a0
Eliot Jordan <eliotj@princeton.edu>
2025-01-09 12:11:11 -0600

    Remove harmful language form and modal

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,15 +5,6 @@ class ContactMailer < ApplicationMailer
     mail(to: @form.routed_mail_to, from: @form.email, subject: "Suggest a Correction")
   end
 
-  def report
-    @form = params[:form_class].new(params[:form_params])
-    if @form.email.blank?
-      mail(to: @form.routed_mail_to, from: "anonymous@princeton.edu", subject: "Reporting Harmful Language")
-    else
-      mail(to: @form.routed_mail_to, from: @form.email, subject: "Reporting Harmful Language")
-    end
-  end
-
   def contact
     @form = params[:form_class].new(params[:form_params])
     mail(to: @form.routed_mail_to, from: @form.email, subject: @form.email_subject)
