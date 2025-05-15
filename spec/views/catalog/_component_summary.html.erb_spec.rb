c9e1f097103608e27b096ef7969e7e18e468ad1f
Oh, Hey! <shaun@sdellis.com>
2025-05-15 08:06:25 -0400

    adds warning to restricted components

--- spec/views/catalog/_component_summary.html.erb_spec.rb
+++ spec/views/catalog/_component_summary.html.erb_spec.rb
@@ -18,4 +18,39 @@ describe "catalog/_component_summary" do
       expect(rendered).not_to have_selector(".document-title .document-extent")
     end
   end
+  context "when the component has restricted content" do
+    let(:document) do
+      {
+        "accessrestrict_ssm" => ["This item is restricted. These materials will be available in 2075."],
+        "access_ssi" => ["restricted"]
+      }
+    end
+    let(:solr_document) { SolrDocument.new(document) }
+
+    it "displays a warning above the description about restricted materials" do
+      stub_template "catalog/_show_upper_metadata_default" => ""
+      assign :document, solr_document
+      render
+
+      expect(response.body).to have_css(".access-restrictions-warning", text: "This item is restricted.") 
+      
+    end
+
+    it "truncates long text to 25 characters for the warning" do
+      stub_template "catalog/_show_upper_metadata_default" => ""
+      assign :document, solr_document
+      render
+
+      expect(response.body).to have_css(".access-restrictions-warning", text: /^.{0,25}$/) 
+    end
+
+    it "adds a link and modal containing the full text of conditions governing access" do
+      stub_template "catalog/_show_upper_metadata_default" => ""
+      assign :document, solr_document
+      render
+
+      expect(rendered).to have_button "Read full Conditions Governing Access"
+      expect(response.body).to have_css(".modal-body", text: "This item is restricted. These materials will be available in 2075.") 
+    end
+  end
 end
