00630e16161278cfeca28e88978f906e9e215bbc
hackartisan <845363+hackartisan@users.noreply.github.com>
2025-12-17 11:09:48 -0500

    Use no-reply as from if an email is not given
    refs #1621

--- spec/controllers/contact_controller_spec.rb
+++ spec/controllers/contact_controller_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe ContactController do
         expect(response.body).to have_content "Message can't be blank"
       end
     end
+
     context "when given valid params" do
       it "returns a 200 and sends an email appropriately" do
         post :suggest, params: {
@@ -39,8 +40,35 @@ RSpec.describe ContactController do
         expect(delivery.body).to include "This record needs more science."
         expect(delivery.body).to include "http://example.com/example"
       end
+
+      it "doesn't require an email address" do
+        post :suggest, params: {
+          suggest_a_correction_form: {
+            "name" => "Bill Nye",
+            "email" => "",
+            "box_number" => "1",
+            "message" => "This record needs more science.",
+            "context" => "http://example.com/example",
+            "location_code" => "mudd"
+          }
+        }
+
+        expect(response.status).to eq 200
+        expect(response.body).to have_field "Name", with: ""
+        expect(response.body).to have_content "Thank you for submitting"
+
+        expect(ActionMailer::Base.deliveries.length).to eq 1
+        delivery = ActionMailer::Base.deliveries.first
+        expect(delivery.subject).to eq "Suggest a Correction"
+        expect(delivery.to).to eq ["suggestacorrection@princeton.libanswers.com"]
+        expect(delivery.from).to eq ["no-reply@localhost"]
+        expect(delivery.body).to include "Bill Nye"
+        expect(delivery.body).to include "This record needs more science."
+        expect(delivery.body).to include "http://example.com/example"
+      end
     end
   end
+
   describe "POST question" do
     context "when given invalid data AJAX data" do
       it "returns the form re-rendered" do
@@ -52,6 +80,7 @@ RSpec.describe ContactController do
         expect(response.body).to have_content "Email can't be blank"
       end
     end
+
     context "when given valid params" do
       it "returns a 200 and sends an email appropriately" do
         post :question, params: {
