848e59db6852c8a83eebe4bb08470e3c4343b008
hackartisan <845363+hackartisan@users.noreply.github.com>
2025-12-18 10:55:47 -0500

    Add new 2nd staging box
    refs #1622

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,3 +2,4 @@
 set :rails_env, "staging"
 
 server "pulfalight-staging1.princeton.edu", user: "deploy", roles: %w[app db web worker]
+server "pulfalight-staging2.princeton.edu", user: "deploy", roles: %w[app web worker]
