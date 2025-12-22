f5a367799a42700494ca9e4d99c8dbd733ec5e96
Oh, Hey! <shaun@sdellis.com>
2025-12-22 16:52:05 -0500

    Got tests passing
    Co-authored-by: Eliot Jordan <eliotjordan@users.noreply.github.com>

--- app/services/oauth_service.rb
+++ app/services/oauth_service.rb
@@ -11,37 +11,37 @@ class OAuthService
   end
 
   def new_token
-    token = JSON.parse(response.body)['access_token']
+    token = JSON.parse(response.body)["access_token"]
     raise CouldNotGenerateOAuthToken unless token
     token
   end
 
   def expiration_time
-    validity_in_seconds = JSON.parse(response.body)['expires_in']
+    validity_in_seconds = JSON.parse(response.body)["expires_in"]
     validity_in_seconds.seconds.from_now - 1.hour
   end
 
     private
 
-      attr_reader :endpoint, :service
+  attr_reader :endpoint, :service
 
-      def client_id
-        configuration[:client_id]
-      end
+  def client_id
+    configuration[:client_id]
+  end
 
-      def client_secret
-        configuration[:client_secret]
-      end
+  def client_secret
+    configuration[:client_secret]
+  end
 
-      def response
-        @response ||= Net::HTTP.post_form(endpoint, client_id:, client_secret:, grant_type:)
-      end
+  def response
+    @response ||= Net::HTTP.post_form(endpoint, client_id: client_id, client_secret: client_secret, grant_type: grant_type)
+  end
 
-      def grant_type
-        'client_credentials'
-      end
+  def grant_type
+    "client_credentials"
+  end
 
-      def configuration
-        @configuration ||= Rails.application.config_for(:pulfalight)[service]
-      end
+  def configuration
+    @configuration ||= Rails.application.config_for(:config)[service]
+  end
 end
--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -13,5 +13,5 @@
 
 # These inflection rules are supported but not enabled by default:
 ActiveSupport::Inflector.inflections(:en) do |inflect|
-  inflect.acronym 'OAuth'
+  inflect.acronym "OAuth"
 end
