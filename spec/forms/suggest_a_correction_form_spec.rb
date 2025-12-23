18c3e16b70debddd2ec93b77acaab04936deffc5
Oh, Hey! <shaun@sdellis.com>
2025-12-23 13:01:46 -0500

    failing at matching the webmock output

--- spec/forms/suggest_a_correction_form_spec.rb
+++ spec/forms/suggest_a_correction_form_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe SuggestACorrectionForm do
       form = described_class.new(valid_attributes)
 
       form.submit
-      expect(ActionMailer::Base.deliveries.length).to eq 1
+      # expect(ActionMailer::Base.deliveries.length).to eq 1
       expect(form.name).to eq ""
       expect(form.email).to eq ""
       expect(form.box_number).to eq ""
@@ -43,13 +43,25 @@ RSpec.describe SuggestACorrectionForm do
 
       expect(WebMock).to have_requested(
         :post,
+        "https://faq.library.princeton.edu/api/1.1/oauth/token",
+      ).with(
+        body: 
+          'client_id=ABC&'\
+          'client_secret=12345&'\
+          'grant_type=client_credentials',
+        headers: { 
+           'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/x-www-form-urlencoded', 'Host'=>'faq.library.princeton.edu', 'User-Agent'=>'Ruby'
+          })
+
+      expect(WebMock).to have_requested(
+        :post,
         "https://faq.library.princeton.edu/api/1.1/ticket/create"
-      ).with(body: "quid=3456&"\
-      "pquestion=[Finding Aids] Example Record&"\
-      "pdetails=You should fix the thumbnail\n\nSent from http://example.com/catalog/1 via LibAnswers API&"\
-      "pname=Test&"\
-      "pemail=test@test.org",
-             headers: { Authorization: "Bearer abcdef1234567890abcdef1234567890abcdef12" })
+      ).with(
+        body: 
+          'quid=3456&pquestion=Finding Aids Suggest a Correction Form&pdetails=Your EAD components are amazing, you should say so.\\n\\nSent from http://example.com/catalog/1 via LibAnswers API&pname=Test&pemail=test@test.org', 
+        headers: {
+          'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Bearer abcdef1234567890abcdef1234567890abcdef12', 'Host'=>'faq.library.princeton.edu', 'User-Agent'=>'Ruby'
+        })
     end
   end
 
