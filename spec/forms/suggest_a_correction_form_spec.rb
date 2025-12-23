6baced593aef95da6807cc0a5f1f6c165d6b4c01
Oh, Hey! <shaun@sdellis.com>
2025-12-23 13:02:27 -0500

    rubocop

--- spec/forms/suggest_a_correction_form_spec.rb
+++ spec/forms/suggest_a_correction_form_spec.rb
@@ -43,25 +43,27 @@ RSpec.describe SuggestACorrectionForm do
 
       expect(WebMock).to have_requested(
         :post,
-        "https://faq.library.princeton.edu/api/1.1/oauth/token",
+        "https://faq.library.princeton.edu/api/1.1/oauth/token"
       ).with(
-        body: 
-          'client_id=ABC&'\
-          'client_secret=12345&'\
-          'grant_type=client_credentials',
-        headers: { 
-           'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/x-www-form-urlencoded', 'Host'=>'faq.library.princeton.edu', 'User-Agent'=>'Ruby'
-          })
+        body:
+          "client_id=ABC&"\
+          "client_secret=12345&"\
+          "grant_type=client_credentials",
+        headers: {
+          "Accept" => "*/*", "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3", "Content-Type" => "application/x-www-form-urlencoded", "Host" => "faq.library.princeton.edu", "User-Agent" => "Ruby"
+        }
+      )
 
       expect(WebMock).to have_requested(
         :post,
         "https://faq.library.princeton.edu/api/1.1/ticket/create"
       ).with(
-        body: 
-          'quid=3456&pquestion=Finding Aids Suggest a Correction Form&pdetails=Your EAD components are amazing, you should say so.\\n\\nSent from http://example.com/catalog/1 via LibAnswers API&pname=Test&pemail=test@test.org', 
+        body:
+          'quid=3456&pquestion=Finding Aids Suggest a Correction Form&pdetails=Your EAD components are amazing, you should say so.\\n\\nSent from http://example.com/catalog/1 via LibAnswers API&pname=Test&pemail=test@test.org',
         headers: {
-          'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Bearer abcdef1234567890abcdef1234567890abcdef12', 'Host'=>'faq.library.princeton.edu', 'User-Agent'=>'Ruby'
-        })
+          "Accept" => "*/*", "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3", "Authorization" => "Bearer abcdef1234567890abcdef1234567890abcdef12", "Host" => "faq.library.princeton.edu", "User-Agent" => "Ruby"
+        }
+      )
     end
   end
 
@@ -98,7 +100,7 @@ RSpec.describe SuggestACorrectionForm do
         "pdetails=Your EAD components are amazing, you should say so.\n\nSent from http://example.com/catalog/1 via LibAnswers API&"\
         "pname=Test&"\
         "pemail=test@test.org",
-         headers: { Authorization: "Bearer abcdef1234567890abcdef1234567890abcdef12" })
+               headers: { Authorization: "Bearer abcdef1234567890abcdef1234567890abcdef12" })
       end
     end
     it "routes to wdressel@princeton.edu for engineering library" do
