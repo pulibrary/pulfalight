17a24ad1fe5b93669c5fd93d8836649a28570dfc
Christina Chortaria <actspatial@gmail.com>
2026-07-15 13:07:56 -0400

    Update db/schema.rb after running the servers

--- db/schema.rb
+++ db/schema.rb
@@ -10,58 +10,58 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_19_185157) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_19_185157) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "bookmarks", id: :serial, force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "user_type"
+    t.datetime "created_at", precision: nil, null: false
     t.string "document_id"
     t.string "document_type"
     t.binary "title"
-    t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
+    t.integer "user_id", null: false
+    t.string "user_type"
     t.index ["document_id"], name: "index_bookmarks_on_document_id"
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
   create_table "events", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", precision: nil, null: false
+    t.string "name"
     t.datetime "updated_at", precision: nil, null: false
     t.index ["name"], name: "index_events_on_name", unique: true
   end
 
   create_table "oauth_tokens", force: :cascade do |t|
-    t.string "service"
-    t.string "endpoint"
-    t.string "token"
-    t.datetime "expiration_time"
     t.datetime "created_at", null: false
+    t.string "endpoint"
+    t.datetime "expiration_time"
+    t.string "service"
+    t.string "token"
     t.datetime "updated_at", null: false
   end
 
   create_table "searches", id: :serial, force: :cascade do |t|
+    t.datetime "created_at", precision: nil, null: false
     t.binary "query_params"
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "user_id"
     t.string "user_type"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id"], name: "index_searches_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: nil, null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: nil
-    t.datetime "remember_created_at", precision: nil
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
     t.boolean "guest", default: false
     t.string "provider"
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "reset_password_sent_at", precision: nil
+    t.string "reset_password_token"
     t.string "uid"
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["provider"], name: "index_users_on_provider"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -69,10 +69,10 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_19_185157) do
   end
 
   create_table "xml_caches", force: :cascade do |t|
-    t.string "resource_descriptions_uri"
     t.text "content"
     t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
     t.string "ead_id"
+    t.string "resource_descriptions_uri"
+    t.datetime "updated_at", precision: nil, null: false
   end
 end
