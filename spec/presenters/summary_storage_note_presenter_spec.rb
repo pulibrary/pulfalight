1a86c0b61154f9e3f2ea835d1d6de1a33012ded8
Ryan Laddusaw <rladdusaw@princeton.edu>
2026-07-22 15:18:44 -0400

    Fix storage note clustering for box numbers like M21

--- spec/presenters/summary_storage_note_presenter_spec.rb
+++ spec/presenters/summary_storage_note_presenter_spec.rb
@@ -118,6 +118,24 @@ RSpec.describe SummaryStorageNotePresenter do
           )
         end
       end
+      context 'when the storage note has a lot of consecutive box numbers of the form A00' do
+        let(:values) do
+          {
+            "summary_storage_note_ssm": [
+              '{"Firestone Library (scamss)":["Boxes 1-2; H1; H2; H3; L1; L2; L3; L4; L5; L6; L7; L8; L9; L10; L11; L12; L13; L14; L15; L16; L17; L18; L19; L20; L21; L22; L23; L24; L25; L26; L27; L28; L29; L30; L31; L32; L33; L34; L35; L36; L37; M1; M2; M3; M4; M5; M6; M7; M8; M9; M10; M11; M12; M13; M14; M15; M16; M17; M18; M19; M20; M21; M22; M23; M24; M25; M26; M27; M28; M29; M30; M31; M32; M33; M34; M35; M36; M37; M38; M39; M40; M41; M42; M43; M44; M45; M46; M47; M48; M49; M50; M51; M52; M53; M54; M55; M56; M57; M58; M59; M60; M61; M62; M63; M64; M65; M66; M67; M68; M69; M70; M71; M72; M73; M74;"]}'
+            ]
+          }
+        end
+        
+        it 'collapses box numbers with the form A00' do
+          expect(ssnote.render).to eq(
+            ["<dl class=\"storage-notes\">",
+             "<dt>Firestone Library (scamss)</dt>",
+             "<dd>Boxes H1 to H3, L1 to L37, M1 to M74</dd>",
+             "</dl>"].join
+          )
+        end
+      end
     end
   end
 end
