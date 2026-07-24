6302852fef864e0b6f301fb28b72ae37418d57cf
Eliot Jordan <eliot.jordan@gmail.com>
2026-07-24 15:34:34 -0500

    Only display the box and folder sort when a collection is selected

--- spec/requests/sorting_spec.rb
+++ spec/requests/sorting_spec.rb
@@ -20,5 +20,13 @@ describe "search result sorting" do
       # Has no box and folder, so does not appear in the top results
       expect(ids).not_to include("MC221_c0001")
     end
+
+    it "displays the box and folder sort option only when a collection is selected" do
+      get "/catalog", params: { q: "diary" }
+      expect(response.body).not_to include("folder number (ascending)")
+
+      get "/catalog", params: { q: "diary", f: { collection_sim: ["Harold B. Hoskins Papers, 1822-1982"] } }
+      expect(response.body).to include("folder number (ascending)")
+    end
   end
 end
