f8a2a6ff5dcae9a763f2871d478539ba4b9d96e8
Trey Pendragon <tpendragon@princeton.edu>
2026-07-28 13:26:42 -0700

    Fix test.
    Supplement to #1783
    
    This also constraints the model for long bioghists.

--- spec/features/catalog_view_spec.rb
+++ spec/features/catalog_view_spec.rb
@@ -591,7 +591,7 @@ describe "viewing catalog records", type: :feature, js: true do
       visit "/catalog/C0140_c03353"
 
       expect(page).to have_link "Matappeas"
-      click_button "(Historical Information)"
+      click_button "(Historical Information)", match: :first
       expect(page).to have_content("Matappeas was a Lenape sachem of Toponemus.")
     end
   end
